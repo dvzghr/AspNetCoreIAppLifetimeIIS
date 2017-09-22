dccb77e717f44903edd3a001488bf9c631cae38f
thshr <ths_hr@hotmail.com>
2017-09-22 17:53:36 +0200

    added IIS hosting option

--- AspNetCoreIAppLifetimeIIS/Controllers/ValuesController.cs
+++ AspNetCoreIAppLifetimeIIS/Controllers/ValuesController.cs
@@ -2,43 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace AspNetCoreIAppLifetimeIIS.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly ILogger<ValuesController> logger;
+
+        public ValuesController(IApplicationLifetime lifetime, ILogger<ValuesController> logger)
+        {
+            this.logger = logger;
+
+            lifetime.ApplicationStarted.Register(() => OnAppEvent("*** STARTED! ***"));
+            lifetime.ApplicationStopping.Register(() => OnAppEvent("*** STOPPING.... ***"));
+            lifetime.ApplicationStopped.Register(() => OnAppEvent("*** STOPPED!!! ***"));
+        }
+
+        private void OnAppEvent(string msg)
+        {
+            logger.LogWarning(msg);
+            System.Threading.Thread.Sleep(3000);
+        }
+
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return "DONE!";
         }
     }
 }
